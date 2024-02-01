436afb7da852043eaa4295f366d6fcd2fc13c1ee
Heyson Betancourt <146369401+heysonbr@users.noreply.github.com>
2024-02-01 18:58:22 +0000

    acabado

--- src/js/component/input.jsx
+++ src/js/component/input.jsx
@@ -1,86 +1,81 @@
 import React, { useState } from "react";
 
 const TaskList = () => {
-  // Definimos el estado para la tarea actual, la lista de tareas, si estamos editando, el ID de la tarea que estamos editando y la alerta
   const [task, setTask] = useState("");
   const [taskList, setTaskList] = useState([]);
   const [isEditing, setIsEditing] = useState(false);
   const [editID, setEditID] = useState(null);
   const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
 
-  // Definimos la función que se ejecutará cuando se envíe el formulario
   const handleSubmit = (e) => {
-    e.preventDefault(); // Evitamos que la página se recargue
+    e.preventDefault();
     if (!task) {
-      // Si la tarea está vacía, mostramos una alerta
       setAlert({
         show: true,
         msg: "Por favor, introduce una tarea",
         type: "danger",
       });
     } else if (task && isEditing) {
-      // Si estamos editando, actualizamos la tarea en la lista
       setTaskList(
         taskList.map((item) => {
           if (item.id === editID) {
-            return { ...item, task }; // Si el ID coincide, actualizamos la tarea
+            return { ...item, task };
           }
-          return item; // Si no, devolvemos la tarea tal cual
+          return item;
         })
       );
-      // Reseteamos el estado
       setTask("");
       setEditID(null);
       setIsEditing(false);
       setAlert({ show: true, msg: "Valor cambiado", type: "success" });
     } else {
-      // Si no estamos editando, añadimos la tarea a la lista
       const newTask = { id: new Date().getTime().toString(), task };
       setTaskList([...taskList, newTask]);
-      // Reseteamos el estado
       setTask("");
       setAlert({
         show: true,
         msg: "Tarea añadida a la lista",
         type: "success",
       });
-      console.log(taskList);
     }
   };
 
-  // Renderizamos el componente
   return (
-    <div className="container">
-      {alert.show && <p className={`alert alert-${alert.type}`}>{alert.msg}</p>}{" "}
-      {/* Mostramos la alerta si es necesario */}
-      <div className="">
-        <form onSubmit={handleSubmit} className="form">
-          {" "}
-          {/* Cuando se envíe el formulario, llamamos a handleSubmit */}
-          <input
-            type="text"
-            placeholder="Escribe una tarea"
-            value={task}
-            onChange={(e) => setTask(e.target.value)} // Cuando cambie el valor del input, actualizamos el estado
-          />
-          <button type="submit">{isEditing ? "Editar" : "Añadir"}</button>
-          {/* El texto del botón cambia si estamos editando */}
-        </form>
-        <div>
-          {taskList.map(
-            (
-              item // Para cada tarea en la lista, renderizamos un div con la tarea y los botones de editar y eliminar
-            ) => (
-              <div
+    <div className="container mt-5">
+      {alert.show && (
+        <div className={`alert alert-${alert.type}`} role="alert">
+          {alert.msg}
+        </div>
+      )}
+      <div className="card">
+        <div className="card-body">
+          <form onSubmit={handleSubmit}>
+            <div className="input-group mb-3">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Escribe una tarea"
+                value={task}
+                onChange={(e) => setTask(e.target.value)}
+              />
+              <div className="input-group-append">
+                <button className="btn btn-outline-primary" type="submit">
+                  {isEditing ? "Editar" : "Añadir"}
+                </button>
+              </div>
+            </div>
+          </form>
+          <ul className="list-group list-group-flush d-flex justify-content-between align-items-center">
+            {taskList.map((item) => (
+              <li
                 key={item.id}
-                className="task-item row d-flex justify-content-center"
+                className="list-group-item d-flex justify-content-between align-items-center"
               >
-                <h5 className="d-flex justify-content-center align-items-center m-3">
-                  {item.task}
+                {item.task}
+                <div>
                   <button
-                    className="edit-button m-1"
+                    className="btn btn-outline-success mr-2 hide-button"
                     onClick={() => {
-                      // Cuando se haga click en el botón, actualizamos el estado para editar la tarea
                       setTask(item.task);
                       setEditID(item.id);
                       setIsEditing(true);
@@ -89,9 +84,8 @@ const TaskList = () => {
                     Editar
                   </button>
                   <button
-                    className="delete-button m-1"
+                    className="btn btn-outline-danger hide-button"
                     onClick={() => {
-                      // Cuando se haga click en el botón, eliminamos la tarea de la lista
                       setTaskList(
                         taskList.filter((task) => task.id !== item.id)
                       );
@@ -104,20 +98,17 @@ const TaskList = () => {
                   >
                     Eliminar
                   </button>
-                </h5>
-              </div>
-            )
-          )}
+                </div>
+              </li>
+            ))}
+          </ul>
         </div>
-        <div>
-          <p>
-            {taskList.length > 0
-              ? `Hay ${taskList.length} ${
-                  taskList.length === 1 ? "tarea" : "tareas"
-                }` // Mostramos el número de tareas en la lista
-              : "Agrega Tareas"}{" "}
-            {/*Si no hay tareas, mostramos un mensaje para que se añadan*/}
-          </p>
+        <div className="card-footer text-muted">
+          {taskList.length > 0
+            ? `Hay ${taskList.length} ${
+                taskList.length === 1 ? "tarea" : "tareas"
+              }`
+            : "Agrega Tareas"}
         </div>
       </div>
     </div>
